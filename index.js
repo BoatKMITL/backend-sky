3fe568197d215166d7b2acb3464ee5d66c1ef330
Boat <88446719+BoatKMITL@users.noreply.github.com>
2025-03-19 03:42:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const path = require("path");
 const fs = require("fs");
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 const db = mysql.createConnection({
     host: "th257.ruk-com.in.th",
